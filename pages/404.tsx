26b5c3810a8c8ae1402a43e4f145233d80ba241f
Mustafa Aljadery <mustafaaljadery1@gmail.com>
2022-07-25 12:08:14 -0700

    working on ReadME

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,10 +1,20 @@
 import type { NextPage } from 'next';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const Error: NextPage = () => {
   return (
-    <div>
-      <h1>There was an error</h1>
-    </div>
+    <>
+      <Header />
+      <main className="flex flex-col items-center">
+        <div className="w-[45%] mb-32">
+          <h2 className="mt-16 font-bold text-4xl">
+            There was an Error
+          </h2>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 };
 
